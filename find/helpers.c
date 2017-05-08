77a8c8d86353ce3368ee22c098c084922b52f47e
Akshay kamboj <akskamboj1998@gmail.com>
2017-05-08 19:26:15 +0000

    sorting code added

--- find/helpers.c
+++ find/helpers.c
@@ -8,8 +8,27 @@
 
 #include "helpers.h"
 
-int main{
-    return 0;
+
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    for ( int i = 0 ; i < n - 1 ; i++)
+    { 
+        int first = i ;
+        for ( int j = i + 1 ; j < n ; j++)
+        {
+            if (values[first] > values[j])
+            {
+                first = j ;
+            }
+        }
+          
+        int temp = values[i] ;
+        values[i] = values[first] ;
+        values[first] = temp ;
+    }
+    return ;
 }
-    
-    
