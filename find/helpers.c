1ae47efd0d8e62c4c9eccb0155e5d53477887d1a
Akshay kamboj <akskamboj1998@gmail.com>
2017-05-08 19:32:40 +0000

    same comments again added

--- find/helpers.c
+++ find/helpers.c
@@ -8,7 +8,9 @@
 
 #include "helpers.h"
 
-
+/**
+ * Returns true if value is in array of n values, else false.
+ */
 bool search(int value, int values[], int n)
 {
     int start = 0 ;
@@ -44,7 +46,9 @@ bool search(int value, int values[], int n)
     return false;
 }
 
-
+/**
+ * Sorts array of n values.
+ */
 void sort(int values[], int n)
 {
     for ( int i = 0 ; i < n - 1 ; i++)
